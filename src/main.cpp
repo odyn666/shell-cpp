e2c8f3d4c09f2346fa51a3a1c8180f7119d3fa27
odyn <lambertmorhen@wp.pl>
2025-02-19 22:50:40 +0100

    rework of input handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,72 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>   // For std::istringstream
+#include <algorithm> // For std::find
+
+// Function to split a string into words
+std::vector<std::string> splitString(const std::string& input) {
+	std::vector<std::string> splited;
+	std::istringstream iss(input);
+	std::string word;
+
+	while (iss >> word) {
+		splited.push_back(word);
+	}
+
+	return splited;
+}
+
+int handleExitCommand(std::string& arg) {
+	try
+	{
+		return std::stoi(arg);
+	}
+	catch (const std::exception&)
+	{
+		std::cout << " wrong argument" << std::endl;
+		return -1;
+	}
+}
 
 int main() {
-	// Flush after every std::cout / std:cerr
+	// Flush after every std::cout / std::cerr
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
 
+	std::vector<std::string> knownCommands{ "exit" };
+	std::vector<std::string> args{};
 
-	while (true)
-	{
+	while (true) {
 		std::cout << "$ ";
+
 		std::string input;
 		std::getline(std::cin, input);
-		if (input == "exit 0")
+		std::vector<std::string> splitedCommands = splitString(input);
+
+		//set arguments
+		args = splitedCommands;
+		args.erase(args.begin());// removing first elemenct since its command not argument
+
+		// finds command in vector and Ensures we don't access splitedCommands[0] if the input is empty
+		bool isKnownCommand = !splitedCommands.empty() &&
+			std::find(knownCommands.begin(), knownCommands.end(), splitedCommands[0]) != knownCommands.end();
+
+		if (isKnownCommand)
 		{
-			return 0;
+
+			if (splitedCommands[0] == "exit")
+			{
+				int exitCode = handleExitCommand(args[0]);
+				if (exitCode != -1)
+
+					return exitCode; // returning with provided exit code
+			}
 		}
 		else
 		{
 			std::cout << input << ": command not found" << std::endl;
-
+			args.clear();
 		}
 	}
-
 }
