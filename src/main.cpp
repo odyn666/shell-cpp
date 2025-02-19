2ca2b8583f8eedde65194f49437e991d20b9d825
odyn <lambertmorhen@wp.pl>
2025-02-19 21:15:30 +0100

    exit 0 added

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,21 @@ int main() {
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
 
-	// Uncomment this block to pass the first stage
 
 	while (true)
 	{
 		std::cout << "$ ";
 		std::string input;
 		std::getline(std::cin, input);
-		std::cout << input << ": command not found" << std::endl;
+		if (input == "exit 0")
+		{
+			return 0;
+		}
+		else
+		{
+			std::cout << input << ": command not found" << std::endl;
 
+		}
 	}
 
 }
